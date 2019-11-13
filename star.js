3b7a1aa81bf28eb320ffb5c56db5b69b3726aef5
Ruthra13 <57712694+Ruthra13@users.noreply.github.com>
2019-11-13 18:26:34 +0530

    Create star.js

---
+++ star.js
@@ -0,0 +1,20 @@
+<html>
+<head>
+<script>
+function myfunc()
+{
+star = "";
+i = document.getElementById("ta").value;
+for(j=0;j<=i;j++)
+{
+star = star + "*" +"<br>";
+}
+document.getElementById("output").innerHTML = star;
+}
+</script>
+</head>
+<body>
+<input type="text" id="ta" onkeyup="myfunc()"></input>
+<p id="output"></p>
+</body>
+</html>
